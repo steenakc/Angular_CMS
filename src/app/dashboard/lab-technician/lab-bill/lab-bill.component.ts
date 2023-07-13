ac050ea041bdc6463513e3b3d10a25775822fab3
steena <steenakc@gmail.com>
2023-07-13 23:18:38 +0530

    shivas error correction

--- src/app/dashboard/lab-technician/lab-bill/lab-bill.component.ts
+++ src/app/dashboard/lab-technician/lab-bill/lab-bill.component.ts
@@ -1,4 +1,8 @@
+
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AddstockService } from 'src/app/service/addstock.service';
+
 
 @Component({
   selector: 'app-lab-bill',
@@ -7,95 +11,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LabBillComponent implements OnInit {
 
-  constructor() { }
+  appointmentId:number;
+  medicineprescriptionlist:any
+  medicineprescriptionlist1:any
+
+  constructor(private listmedicineprescription:AddstockService,private router: Router,private activatedrouter:ActivatedRoute) { }
+
 
   ngOnInit(): void {
+    this.activatedrouter.paramMap.subscribe(params=>{
+      this.appointmentId=Number(params.get('appointmentId'));
+      console.log(this.appointmentId);
+    })
+    this.listmedicineprescription.prescriptionById(this.appointmentId).subscribe(
+      (result: any[]) => {
+        console.warn(result);
+        this.medicineprescriptionlist = result;
+        this.medicineprescriptionlist1= result[0];
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    );
+    console.log("medicineprescriptionlist")
+  }
+  
+  
+  getTotalPrice(): number {
+    let totalPrice = 0;
+    for (let item of this.medicineprescriptionlist) {
+      totalPrice += item.count * item.medicne.rate;
+    }
+    return totalPrice;
+  }
+  
+
+  cancel(){
+    this.router.navigate(['dashboard/pharmacist/list-medicineprescription']);
+
   }
 
-}
 
 
+  getCurrentDate() {
+    const today = new Date();
+    const day = today.getDate().toString().padStart(2, '0');
+    const month = (today.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-based
+    const year = today.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
 
-
-// import { Component, OnInit } from '@angular/core';
-// import { ActivatedRoute, Router } from '@angular/router';
-// import { AddstockService } from 'src/app/service/addstock.service';
-
-// @Component({
-//   selector: 'app-pharmacist-bill',
-//   templateUrl: './pharmacist-bill.component.html',
-//   styleUrls: ['./pharmacist-bill.component.scss']
-// })
-// export class PharmacistBillComponent implements OnInit {
-  
-//   appointmentId:number;
-//   medicineprescriptionlist:any
-//   medicineprescriptionlist1:any
-
-//   constructor(private listmedicineprescription:AddstockService,private router: Router,private activatedrouter:ActivatedRoute) { }
-
-
-//   ngOnInit(): void {
-//     this.activatedrouter.paramMap.subscribe(params=>{
-//       this.appointmentId=Number(params.get('appointmentId'));
-//       console.log(this.appointmentId);
-//     })
-//     this.listmedicineprescription.prescriptionById(this.appointmentId).subscribe(
-//       (result: any[]) => {
-//         console.warn(result);
-//         this.medicineprescriptionlist = result;
-//         this.medicineprescriptionlist1= result[0];
-//       },
-//       (error: any) => {
-//         console.log(error);
-//       }
-//     );
-//     console.log("medicineprescriptionlist")
-//   }
-  
-  
-//   getTotalPrice(): number {
-//     let totalPrice = 0;
-//     for (let item of this.medicineprescriptionlist) {
-//       totalPrice += item.count * item.medicne.rate;
-//     }
-//     return totalPrice;
-//   }
-  
-
-//   cancel(){
-//     this.router.navigate(['dashboard/pharmacist/list-medicineprescription']);
-
-//   }
-
-
-
-//   getCurrentDate() {
-//     const today = new Date();
-//     const day = today.getDate().toString().padStart(2, '0');
-//     const month = (today.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-based
-//     const year = today.getFullYear();
-//     return `${day}/${month}/${year}`;
-//   }
-
-//   disableprescription(medicnePrescriptionId:number){
-//     console.warn("medicnePrescriptionId",medicnePrescriptionId)
-//     this.listmedicineprescription.deletePrescription(medicnePrescriptionId).subscribe((result)=>{
+  disableprescription(medicnePrescriptionId:number){
+    console.warn("medicnePrescriptionId",medicnePrescriptionId)
+    this.listmedicineprescription.deletePrescription(medicnePrescriptionId).subscribe((result)=>{
       
-//       if(result){
+      if(result){
         
-//       }
-//       alert("Medicine is deleted successfully");
+      }
+      alert("Medicine is deleted successfully");
    
-//     })
-//     setTimeout(()=>{
+    })
+    setTimeout(()=>{
       
-//     },3000);
-//   }
+    },3000);
+  }
 
   
   
 
-// }
-
-
+}
